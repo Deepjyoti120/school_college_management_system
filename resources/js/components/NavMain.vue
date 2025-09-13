71eeceedd1fded031b0a7d9ce51a18b908427f5e
Deepjyoti120 <deepjyoti120281@gmail.com>
2025-09-13 18:15:51 +0530

    done

--- resources/js/components/NavMain.vue
+++ resources/js/components/NavMain.vue
@@ -20,15 +20,15 @@ defineProps<{
     items: {
         title: string
         items?: {
-        title: string
-        url: string
-        icon?: LucideIcon
-        isActive?: boolean
-        items?: {
             title: string
             url: string
+            icon?: LucideIcon
+            isActive?: boolean
+            items?: {
+                title: string
+                url: string
+            }[]
         }[]
-    }[]
     }[]
 }>()
 const page = usePage();
