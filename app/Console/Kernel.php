76267088b439465ec317cf4ac7135edddea26d86
Deepjyoti120 <deepjyoti120281@gmail.com>
2025-11-13 21:07:24 +0530

    now not required timze zone

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('app:attendance-generate-teacher')
-            ->dailyAt('04:00')->timezone('Asia/Kolkata')
+            ->dailyAt('04:00')
+            // ->timezone('Asia/Kolkata')
             ->onFailure(function () {
                 //
             });
