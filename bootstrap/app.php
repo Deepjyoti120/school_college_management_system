620ca7ca8dc955afcdb6590c8d5fc1e63f7fa22f
Deepjyoti120 <deepjyoti120281@gmail.com>
2025-11-13 21:11:25 +0530

    perfect

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
-use App\Console\Kernel as ConsoleKernel; // ✅ Add this
+use App\Console\Kernel as ConsoleKernel;
 
 $app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
